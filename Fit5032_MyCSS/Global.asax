30869b5431f72eec10abcaf4457f3d8a9ca06855
xinhao <3310507620@qq.com>
2022-07-01 17:45:58 +0800

    first attempt

---
+++ Fit5032_MyCSS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fit5032_MyCSS.MvcApplication" Language="C#" %>
---
+++ Fit5032_MyCSS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fit5032_MyCSS.Startup))]
+namespace Fit5032_MyCSS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
